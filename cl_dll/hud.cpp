f24fd5fe0d2d7e4468eae157f6192e65efcaa284
executλble <execut4ble@gmail.com>
2017-03-17 19:52:29 +0200

    Initial cl_scores #2
    Missing include

--- cl_dll/hud.cpp
+++ cl_dll/hud.cpp
@@ -34,6 +34,7 @@
 
 #include "forcemodel.h"
 #include "steam_id.h"
+#include "hud_scores.h"
 
 hud_player_info_t	 g_PlayerInfoList[MAX_PLAYERS+1];	   // player info from the engine
 extra_player_info_t  g_PlayerExtraInfo[MAX_PLAYERS+1];   // additional player info sent directly to the client dll
